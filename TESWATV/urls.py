44f4f057cf1a945888f53af3ccefa549f2b036d1
Thanzeel Hassan <46446324+thanzeelhassan@users.noreply.github.com>
2020-05-18 07:16:06 +0530

    admin backend
    finished all the basic admin backend functions

--- TESWATV/urls.py
+++ TESWATV/urls.py
@@ -1,25 +1,18 @@
-"""TESWATV URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
 from rating_app import views
 from django.views.generic import TemplateView
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('djangoadmin/', admin.site.urls),
+    path('evaluation_progress/', views.evaluation_progress),
+    path('details/', views.details),
+    path('overall/', views.overall),
+    path('database/', views.database),
+    path('delete_database/', views.delete_database),
+    path('save_database/',views.save_database),
+    path('save_database_1/', views.save_database_1),
+    path('save_database_2/', views.save_database_2),
     path('update_database/', views.update_database),
+    path('admin/', views.admin),
 ]
--- rating_app/views.py
+++ rating_app/views.py
@@ -2,6 +2,101 @@ import csv,io
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from rating_app.models import credited_courses_table,rating_table
+from . import forms
+
+def admin(request):
+    template="admin.html"
+    return render(request,template)
+
+def database(request):
+    template="database.html"
+    return render(request,template)
+
+def save_database(request):
+    template="save_database.html"
+    return render(request,template)
+
+def evaluation_progress(request):
+    if request.method=="GET":
+        template="evaluation_progress.html"
+        form=forms.progress()
+        return render(request,template,{'form':form})
+    else:
+        form=forms.progress(request.POST)
+        if form.is_valid():
+            status=form.cleaned_data['Status']
+            department=form.cleaned_data['Department']
+            if credited_courses_table.objects.filter(feedback_status=status).exists() :
+                q1 = credited_courses_table.objects.filter(roll_no__endswith=department)
+                q2 = q1.filter(feedback_status=status).values('roll_no')
+                return HttpResponse(q2)
+            else :
+                return HttpResponse('No matching rows')
+
+def details(request):
+    if request.method=="GET":
+        template="details.html"
+        form=forms.details()
+        return render(request,template,{'form':form})
+    else:
+        form=forms.details(request.POST)
+        if form.is_valid():
+            a=form.cleaned_data['faculty_name']
+            b=form.cleaned_data['course_name']
+            if rating_table.objects.filter(faculty_name=a,course_name=b).exists() :
+                abcd = rating_table.objects.get(faculty_name=a,course_name=b)
+                ans=abcd.question_1+abcd.question_2+abcd.question_3+abcd.question_4+abcd.question_5+abcd.question_6+abcd.question_7
+                ans=ans/7
+                ans=ans/abcd.count
+                perc=ans/5
+                perc=perc*100
+                text = str(perc) + "%"
+                return render(request,'detailed_statistics.html',{'text':text,'abcd':abcd})
+            else :
+                return HttpResponse('Does not exist')
+
+def overall(request):
+    num1=0
+    for a in rating_table.objects.all():
+        num1=num1+a.count
+    num2=0
+    for b in credited_courses_table.objects.all():
+        num2=num2+1
+    num3=0
+    for c in rating_table.objects.exclude(count=0):
+        num3=num3+1
+    num4=0
+    for d in rating_table.objects.exclude(count=0):
+        num4=num4+1
+    num5=0
+    for e in credited_courses_table.objects.filter(feedback_status=False):
+        num5=num5+1
+    return render(request,'overall_statistics.html',{'num1':num1,'num2':num2,'num3':num3,'num4':num4,'num5':num5})
+
+def save_database_1(request):
+    response1 = HttpResponse(content_type='text/csv')
+    response1['Content-Disposition'] = 'attachment; filename="credited_courses_table.csv"'
+    writer = csv.writer(response1)
+    writer.writerow(['roll_no', 'faculty_name', 'course_name', 'feedback_status'])
+    rows = credited_courses_table.objects.all()
+    for row in rows:
+        writer.writerow([row.roll_no,row.faculty_name,row.course_name,row.feedback_status])
+    return response1
+
+def save_database_2(request):
+    response2 = HttpResponse(content_type='text/csv')
+    response2['Content-Disposition'] = 'attachment; filename="rating_table.csv"'
+    writer = csv.writer(response2)
+    writer.writerow(['faculty_name', 'course_name', 'question_1', 'question_2', 'question_3', 'question_4', 'question_5', 'question_6', 'question_7', 'count'])
+    rows = rating_table.objects.all()
+    for row in rows:
+        writer.writerow([row.faculty_name,row.course_name,row.question_1,row.question_2,row.question_3,row.question_4,row.question_5,row.question_6,row.question_7,row.count])
+    return response2
+
+def delete_database(request):
+    credited_courses_table.objects.all().delete()
+    rating_table.objects.all().delete()
+    return HttpResponse('all tables cleared')
 
 def update_database(request):
     template="update_database.html"
@@ -39,7 +134,3 @@ def update_database(request):
     'order' : 'Successfully uploaded'
     }
     return HttpResponse('Successfully uploaded')
-    #return render(request,template,context)
-
-def evaluation_progress(request):
-    template="evaluation_progress.html"
